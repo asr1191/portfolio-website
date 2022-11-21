a74fbae81ae0c270c4c3c965d2477057a1e6383e
Amal S Raj <amalsraj95@gmail.com>
2022-11-22 00:32:54 +0530

    blog init

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,17 @@
+import Card from 'components/Card';
+import SearchBar from 'components/Searchbar';
+
 export default function Blog() {
     return (
-        <div className='bg-ht-gray-700'>
-            <main className='mx-auto bg-ht-gray-700 max-w-3xl'>
-                <section className="h-[200vh]">
-                </section>
+        <div className='h-screen bg-ht-gray-700'>
+            <main className="max-w-3xl mx-auto">
+                <Card 
+                    title={'Blog'}
+                    subtitle={`I've been writing online since #YEAR, mostly about web development and tech careers. In total, I've written #NO_OF_ARTICLES articles on my blog. Use the search below to filter by title.`}
+                    className={'mt-8'}
+                >
+                    <SearchBar />
+                </Card>
             </main>
         </div>
     )
