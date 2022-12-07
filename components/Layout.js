31e1a2bf5860d3b77ca0773bd8bec385137722fd
Amal S Raj <amalsraj95@gmail.com>
2022-12-07 23:14:05 +0530

    setup background in layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,20 @@
 import NavBar from "@components/NavBar";
 
+export function Footer() {
+    return (
+        <div className='mt-8 '>
+            <div className='border-gray-400 border-t-[1px] max-w-5xl mx-auto'></div>
+            <p className='py-4 mx-auto text-center text-ht-gray-600'>Amal S Raj</p>
+        </div>
+    )
+}
+
 export default function Layout({ children }) {
     return (
-        <div className="bg-ht-gray-700">
+        <div className="min-h-screen bg-ht-gray-700">
             <NavBar />
             {children}
+            <Footer />
         </div>
     )
 }
