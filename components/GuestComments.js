6cb2b2fa27e50cf6c6629c6d0cdc41d80eda93bd
Amal S Raj <amalsraj95@gmail.com>
2022-12-15 22:09:48 +0530

    Add temporary key to GuestComment map

--- components/GuestComments.js
+++ components/GuestComments.js
@@ -4,8 +4,8 @@ export default function GuestComment() {
     return (
         <div>
             {comments.map((comment) => (
-                <div className='p-4 mt-4 rounded-lg bg-ht-gray-800'>
-                    <p className='text-2xl  text-ht-gray-500'>{comment.content}</p>
+                <div key={comment.time} className='p-4 mt-4 rounded-lg bg-ht-gray-800'>
+                    <p className='text-2xl text-ht-gray-500'>{comment.content}</p>
                     <p><span className='mt-4 text-gray-500'>{comment.name}</span><span className='mt-4 ml-4 text-gray-700'>/ {comment.time}</span></p>
                 </div>
             ))}
@@ -15,5 +15,5 @@ export default function GuestComment() {
 
 // for comment in comments:
 //     print(comment.name)
-// 
+//
 // commments.map()
